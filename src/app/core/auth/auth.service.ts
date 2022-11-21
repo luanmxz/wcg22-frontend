20c07c04a11f1fad4c7f1f35125262ae1e65f0bf
luanmxz <luanmarcenemxz22@gmail.com>
2022-11-21 11:10:22 -0300

    .

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -21,6 +21,8 @@ export class AuthService {
         'Content-Type': 'application/x-www-form-urlencoded',
         Accept: 'application/x-www-form-urlencoded',
         Origin: 'https://https://wcg22.netlify.app/',
+        'Acess-Control-Allow-Headers':
+          'Origin, Content-Type, Accept, Authorization',
         Authorization:
           'Basic ' + btoa(oauth2_client_id + ':' + oauth2_client_secret),
       }),
